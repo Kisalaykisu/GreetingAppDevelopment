a6bca14afc5ee2921d2abd4512f8106e2cccaab3
Kisalay <kisalaykisu@gmail.com>
2022-08-28 01:38:28 +0530

    UC5 has been added:

---
+++ src/main/java/com/example/greetingappdevelopment/model/Greeting.java
@@ -0,0 +1,34 @@
+package com.example.greetingappdevelopment.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Greeting {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String content;
+    public Greeting() {
+
+    }
+    public Greeting(String content) {
+        this.content =content;
+    }
+    public Greeting(Greeting greeting) {
+        this.id =greeting.id;
+        this.content =greeting.content;
+    }
+    public Greeting(Integer id, String content) {
+        super();
+        this.id = id;
+        this.content = content;
+    }
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
