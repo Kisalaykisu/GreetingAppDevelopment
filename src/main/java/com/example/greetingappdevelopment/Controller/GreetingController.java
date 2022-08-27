625188218593bbb3096134cb78a9bb345b6d4026
Kisalay <kisalaykisu@gmail.com>
2022-08-28 01:12:39 +0530

    UC3 has been added:

---
+++ src/main/java/com/example/greetingappdevelopment/Controller/GreetingController.java
@@ -0,0 +1,46 @@
+package com.example.greetingappdevelopment.Controller;
+
+import com.example.greetingappdevelopment.model.Greeting;
+import com.example.greetingappdevelopment.model.User;
+import com.example.greetingappdevelopment.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+@RequestMapping("/web")
+public class GreetingController {
+    private static final String template="Hello %s";
+    private static AtomicLong counter= new AtomicLong();
+
+    @Autowired
+    GreetingService greetingService;
+    @GetMapping("/getGreeting")
+    public Greeting greeting(@RequestParam (value = "name",defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(),String.format(template, name));
+    }
+    @PostMapping("/postGreeting")
+    public Greeting sayHello(@RequestBody Greeting greeting) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, greeting.getContent()));
+    }
+    @PutMapping("/putMapping/{counter}")
+    public Greeting sayHello(@PathVariable long counter,@RequestParam (value="content") String content) {
+        return new Greeting(counter, String.format(template, content));
+    }
+    @GetMapping("/getMessage")
+    public ResponseEntity<String> getMessage(){
+        return new ResponseEntity<String>(greetingService.getMessage(), HttpStatus.OK);
+    }
+    @GetMapping("/getGreetingMessage")
+    public ResponseEntity<String>getGreetingMessage(@RequestParam(value = "fName",defaultValue = "World") String fName,@RequestParam(value = "lName",defaultValue = "") String lName) {
+        return new ResponseEntity<String>(greetingService.getGreetingMessage(fName,lName),HttpStatus.OK);
+    }
+    @PostMapping("/post")
+    public ResponseEntity<String> getGreeting(@RequestBody User user){
+        return new ResponseEntity<String>(greetingService.postMessage(user.getfName(),user.getlName()),HttpStatus.OK);
+    }
+
+}
