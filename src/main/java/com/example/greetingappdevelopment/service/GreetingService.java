2f4a641fd718c4109786185bf5f921b6e9d6b757
Kisalay <kisalaykisu@gmail.com>
2022-08-28 01:25:38 +0530

    UC4 has been added:

---
+++ src/main/java/com/example/greetingappdevelopment/service/GreetingService.java
@@ -0,0 +1,35 @@
+package com.example.greetingappdevelopment.service;
+
+import com.example.greetingappdevelopment.model.Greeting;
+import com.example.greetingappdevelopment.repository.GreetingAppRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class GreetingService {
+    private static String template="Hello %s";
+    @Autowired
+    GreetingAppRepository repo;
+    public String getMessage(){
+        return "Hello World";
+    }
+
+    public String getGreetingMessage(String fName, String lName) {
+        return "Hello "+fName+""+lName;
+    }
+
+    public String postMessage(String fName,String lName) {
+        return "Hello "+fName+""+lName;
+    }
+
+    public Greeting saveMessage(Greeting greeting) {
+        Greeting newGreeting=new Greeting(String.format(template,greeting.getContent()));
+        repo.save(newGreeting);
+        return newGreeting;
+
+    }
+
+    public Greeting saveMessage(Greeting greeting) {
+    }
+}
