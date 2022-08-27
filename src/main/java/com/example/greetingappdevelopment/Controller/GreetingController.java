d6550f542c3a2db172055aec3f46fc03dc333a24
Kisalay <kisalaykisu@gmail.com>
2022-08-28 01:07:11 +0530

    UC2 has been added:

---
+++ src/main/java/com/example/greetingappdevelopment/Controller/GreetingController.java
@@ -0,0 +1,39 @@
+package com.example.greetingappdevelopment.Controller;
+
+
+import com.example.greetingappdevelopment.model.Greeting;
+import com.example.greetingappdevelopment.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+
+
+
+public class GreetingController {
+    private static final String template="Hello %s";
+    private static AtomicLong counter= new AtomicLong();
+
+    @Autowired
+    GreetingService greetingService;
+    @GetMapping("/getGreeting")
+    public Greeting greeting(@RequestParam (value = "name",defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(),String.format(template, name));
+    }
+    @PostMapping("/postGreeting")
+    public Greeting sayHello(@RequestBody Greeting greeting) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, greeting.getContent()));
+    }
+    @PutMapping("/putMapping/{counter}")
+    public Greeting sayHello(@PathVariable long counter,@RequestParam (value="content") String content) {
+        return new Greeting(counter, String.format(template, content));
+    }
+    @GetMapping("/getMessage")
+    public ResponseEntity<String> getMessage(){
+        return new ResponseEntity<String>(greetingService.getMessage(), HttpStatus.OK);
+    }
+
+}
